02abbdc52667538699d2157797d8f65e5d0bd4c2
Uros Todorovic <uros.todorovic-int@utodorovic.levi9.com>
2022-08-10 12:00:55 +0200

    installed nodemon, express, created server and not-found middleware

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('Server running...');
+import express from 'express';
+const app = express();
+
+// Middleware
+import notFoundMiddleware from './middleware/not-found.js';
+
+
+app.get('/', (req, res) => {
+    res.send('Wellcome')
+})
+
+// If non of routes match use not found middleware for any http method
+app.use(notFoundMiddleware);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=>{
+    console.log(`Server listening on port ${port}`);
+})
